d6ae5d3e58d56e789da5209ed0b11bfcdc96165a
Yves Michel ENYEGUE <danielngono002@gmail.com>
2026-05-06 08:32:17 +0200

    Bouton de sidebar mis a jour et fonctionnel

--- src/app/Components/LayoutAdmin/admin-sidebar/admin-sidebar.ts
+++ src/app/Components/LayoutAdmin/admin-sidebar/admin-sidebar.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, Output, EventEmitter, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
@@ -8,10 +8,14 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './admin-sidebar.css',
 })
 export class AdminSidebar {
-   role= signal<number>(0);
-    constructor(){
-      //this.role.set(parseInt(sessionStorage.getItem('role')!)??0); 
-      this.role.set(0); 
-    }
 
+  // ✅ Émet un événement vers le layout parent pour fermer le sidebar
+  @Output() closeSidebar = new EventEmitter<void>();
+
+  role = signal<number>(0);
+
+  constructor() {
+    // this.role.set(parseInt(sessionStorage.getItem('role')!) ?? 0);
+    this.role.set(0);
+  }
 }
